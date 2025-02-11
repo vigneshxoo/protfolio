f315e8adf680baa71e7df54260a5a3c5e43ba181
vigneshxoo <vigneshxoo@gmail.com>
2025-02-11 11:31:07 +0530

    scond push

--- Portfolio/src/components/HeroSection/index.js
+++ Portfolio/src/components/HeroSection/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
 import prot4 from '../../images/prot6.jpg' 
-import myphoto2 from '../../images/prot1.jpg'
+
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 const HeroSection = () => {
